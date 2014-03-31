30c92f65d4f9fd01e72b3b38a0d44a0caefbc1f4
Paul Damer <pdamer@gmail.com>
2014-03-31 14:34:42 -0400

    only decorate if present'

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,4 +2,4 @@ Spree::ProductsController.class_eval do
   helper Spree::ReviewsHelper
 
   [:avg_rating, :reviews_count].each { |attrib| Spree::PermittedAttributes.product_attributes << attrib }
-end
+end if Spree.const_defined? 'ProductsController'
